d6e445409a16063374721b129a6673369727d3e7
Tong Warinthorn <tong.w@finnomena.com>
2022-06-15 10:18:35 +0700

    updated test script

--- doc/wrk-script/create_todo.lua
+++ doc/wrk-script/create_todo.lua
@@ -4,5 +4,6 @@ wrk.headers["Content-Type"] = "application/json"
 counter = 0
 request = function()
     counter = counter + 1
+    -- wrk.format(method, path, headers, body)
     return wrk.format(nil, nil, nil,  '{"task":"attemp #' .. counter .. '"}')
- end
+end
